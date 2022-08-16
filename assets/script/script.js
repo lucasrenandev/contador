e6c0dd3cc29e3bd09175f9dbceaccdf1182d80f7
Lucas Renan <lucasrenandns96@gmail.com>
2022-08-16 16:38:59 -0300

    Contador

---
+++ assets/script/script.js
@@ -0,0 +1,29 @@
+
+const container = document.querySelector(".container")
+const incremento = container.querySelector("#incremento")
+const decremento = container.querySelector("#decremento")
+const zerar = container.querySelector("#zerar")
+const cont = container.querySelector("#contador")
+const icon = container.querySelector(".icon")
+const body = document.querySelector("body")
+
+let contador = 0
+cont.innerHTML = contador
+
+incremento.addEventListener("click", () => {
+    cont.innerHTML = ++contador
+})
+
+decremento.addEventListener("click", () => {
+    cont.innerHTML = --contador
+})
+
+zerar.addEventListener("click", () => {
+    cont.innerHTML = contador = 0
+})
+
+
+// FUNÇÃO DARK MODE
+icon.addEventListener("click", () => {
+    body.classList.toggle("active")
+})
